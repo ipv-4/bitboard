d322f50495b458f4631383dde375ee483680ad66
modaniels <modaniels507@gmail.com>
2026-02-25 18:24:26 +0300

    feat :glued all together,ready for  backend and  db

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <?php 
+    if (isset($extra_js)) {
+        if (is_array($extra_js)) {
+            foreach ($extra_js as $js_file) {
+                echo '<script src="assets/js/' . $js_file . '"></script>' . "\n";
+            }
+        } else {
+        
+            echo '<script src="assets/js/' . $extra_js . '"></script>' . "\n";
+        }
+    }
+    ?>
+
+</body>
+</html>
